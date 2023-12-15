6f84739e6da4978eea28920faaf1a8900d494669
rebel <rebel@Rohit>
2023-12-15 18:51:06 +0530

    new method

---
+++ Hybrid_FrameWork/src/test/java/add.java
@@ -0,0 +1,13 @@
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=23;
+		int b=23;
+		int c=a+b;
+		System. out. println(c);
+	
+	}
+}
